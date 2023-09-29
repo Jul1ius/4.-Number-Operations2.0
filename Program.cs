ecbbdae1026a00817e78c8c3c8da7cbad34ee86d
Julii Abadjiev <117214264+Jul1ius@users.noreply.github.com>
2023-09-29 16:57:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace _4._Number_Operations2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = 0;
+            double n1 = double.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine());
+            string op = Console.ReadLine();
+
+            if (op == "+")
+                total = n1 + n2;
+            else if (op == "-")
+                total = n1 - n2;
+            else if (op == "*")
+                total = n1 * n2;
+            else if (op == "/")
+                total = n1 / n2;
+
+            Console.WriteLine($"{n1} {op} {n2} = {total:f2}");
+        }
+    }
+}
